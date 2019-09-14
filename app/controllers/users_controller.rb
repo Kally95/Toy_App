1d4d1c3ac2abb7fc6009733d9a4728a2006a9619
Ali Kalkanel <kally-95@hotmail.com>
2019-09-14 13:06:20 +0100

    took our .first from method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-    @micropost = @user.microposts.first
+    @micropost = @user.microposts
   end
 
   # GET /users/new
